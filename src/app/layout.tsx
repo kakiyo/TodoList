5f45fbfa9eeb055a0baf3ee6e42a1d096d798332
kakiyo <45170533+kakiyo@users.noreply.github.com>
2025-01-23 18:10:15 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from "next/head";
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+          <Head>
+      <title></title>
+      <div dangerouslySetInnerHTML={{
+        __html: '<script async src="https://www.googletagmanager.com/gtag/js?id=G-138CGPC6W5"></script>\n' +
+            '                <script>\n' +
+            '                    window.dataLayer = window.dataLayer || [];\n' +
+            '                    function gtag(){dataLayer.push(arguments);}\n' +
+            '                    gtag(\'js\', new Date());\n' +
+            '                    gtag(\'config\', \'G-138CGPC6W5\');\n' +
+            '                </script>'
+      }}/>
+
+      <div dangerouslySetInnerHTML={{
+        __html: '<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n' +
+            'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n' +
+            'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n' +
+            '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n' +
+            '})(window,document,\'script\',\'dataLayer\',\'GTM-KTCDXWG\');</script>'
+      }}/>
+
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+      <body>
+         <div dangerouslySetInnerHTML={{
+        __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTCDXWG"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`
+      }}/>
+        {children}
+      </body>
     </html>
   )
 }
