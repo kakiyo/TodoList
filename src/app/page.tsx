0413599fef6681c8db42779eba737f76d5a96780
kakiyo <45170533+kakiyo@users.noreply.github.com>
2025-01-23 17:54:30 +0900

    install gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,19 @@ export default function Home() {
                     '                    gtag(\'config\', \'G-138CGPC6W5\');\n' +
                     '                </script>'
             }}/>
+                 
+            <div dangerouslySetInnerHTML={{
+                __html: '<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n' +
+                    'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n' +
+                    'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n' +
+                    '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n' +
+                    '})(window,document,\'script\',\'dataLayer\',\'GTM-KTCDXWG\');</script>'
+            }}/>
         <main className={styles.main}>
+            <div dangerouslySetInnerHTML={{
+                __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTCDXWG"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`
+            }}/>
            <TodoInput/>
         </main>
         </ConfigProvider>
