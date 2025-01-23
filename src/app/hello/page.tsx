509d01777ac039f5d202ade611fe368356265fe6
kakiyo <45170533+kakiyo@users.noreply.github.com>
2025-01-23 19:50:26 +0900

    Update page.tsx

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -5,8 +5,7 @@ import {useEffect, useState} from "react";
 export default function Home() {
     const [data, setData] = useState();
     useEffect(() => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
+        // @ts-ignore
         fetch("/api/hello").then((res) => setData(res)).catch((err) => console.log(err));
     }, []);
     if (!data){
